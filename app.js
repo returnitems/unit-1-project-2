6b0a1ab694cf8e3000c277d1897a688dd059cf4e
diwanumang <diwan.umang@gmail.com>
2024-04-12 13:11:18 -0400

    Styled page further. Created submit button and functionality. Need to add like/dislike functionality

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const newPhoto = document.getElementById("new-photo");
+const imgForm = document.querySelector("imgForm");
+const urlLabel = document.querySelector("label");
+const urlInput = document.querySelector("#urlInput");
+const submitPic = document.querySelector(".submit");
 
-const hideButton = () => {
+
+const newPic = () => {
     newPhoto.style.display = "none";
+    urlLabel.style.display = "block";
+    urlInput.style.display = "block";
+    submitPic.style.display = "block";
 }
 
-newPhoto.addEventListener("click", hideButton);
+newPhoto.addEventListener("click", newPic);
 
